e7fac67cf8025b826def2aaca3a149467b183cd7
jojann-mnlt <johannmanulat128@gmail.com>
2026-02-05 18:52:07 +0800

    Removed last three lines

--- test.java
+++ test.java
@@ -1,7 +1,5 @@
 public class test {
   public static void main (String[] args) {
     System.out.println("Jacob is a bitch");
-    System.out.println("Johann is the best");
-    System.out.println("End of transmission.");
   }
 }
