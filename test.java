f727f87b7eff749b34bc2610d57899bdcb4d3a32
jojann-mnlt <johannmanulat128@gmail.com>
2026-02-05 18:50:22 +0800

    Second commit

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 public class test {
   public static void main (String[] args) {
     System.out.println("Jacob is a bitch");
+    System.out.println("Johann is the best");
+    System.out.println("End of transmission.");
   }
 }
