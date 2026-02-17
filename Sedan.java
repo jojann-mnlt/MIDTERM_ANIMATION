f5f64f61754e7b5122a8e19baaeb6680edd2a49d
jcb24 <jacoblb237@gmail.com>
2026-02-17 19:25:15 +0800

    Sedan.java

--- Sedan.java
+++ Sedan.java
@@ -5,18 +5,17 @@ public class Sedan extends Car{
     private Color color;
     private Square body, window, roof;
     private Line front_left_pillar, front_right_pillar, middle_pillar, back_left_pillar, back_right_pillar;
-    public Sedan(double k, double j, double s, Color c) {
-        // k = x, j = y, s = size, c = color
-        this.x = k; 
-        this.y = j;
-        this.size = s;
+    public Sedan(double x, double y, double size, Color color) {
+        this.x = x; 
+        this.y = y;
+        this.size = size;
         this.width = size;
         this.height = size*2.4;
-        this.color = c;
+        this.color = color;
     }
     public void draw(Graphics2D g2d){
         g2d.setColor(color);
-
+        // Instantiating car parts
         body = new Square(x, y, width, height, color);
         window = new Square(x+size*0.1, y+size*.6, width*0.8, height*7/12, Color.BLACK);
         roof = new Square(x+size*0.2, y+size, width*0.6, height*1/3, color);
@@ -25,7 +24,7 @@ public class Sedan extends Car{
         middle_pillar = new Line(x+size*0.1, y+size*1.4, x+size*0.9, y+size*1.4, 2, color);
         back_left_pillar = new Line(x+size*0.1, y+size*2, x+size*0.2, y+size*1.8, 2, color);
         back_right_pillar = new Line(x+size*0.9, y+size*2, x+size*0.8, y+size*1.8, 2, color);
-
+        // Rendering car parts
         body.draw(g2d);
         window.draw(g2d);
         roof.draw(g2d);
