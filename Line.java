c5ad6918a44ad2607f784d3b74285a4f081e5a35
jcb24 <jacoblb237@gmail.com>
2026-02-17 17:29:16 +0800

    Line.java & Sedan.java

--- Line.java
+++ Line.java
@@ -3,19 +3,22 @@ import java.awt.geom.*;
 
 public class Line implements DrawingObject {
     private double x, y, x2, y2;
+    private int size;
     private Color color;
 
-    public Line(double x, double y, double x2, double y2, Color color) {
+    public Line(double x, double y, double x2, double y2, int size, Color color) {
         this.x = x;
         this.y = y;
         this.x2 = x2;
         this.y2 = y2;
+        this.size = size;
         this.color = color;
     }
 
     public void draw(Graphics2D g2d) {
         Line2D.Double line = new Line2D.Double(x, y, x2, y2);
         g2d.setColor(color);
+        g2d.setStroke(new BasicStroke(size));
         g2d.draw(line);
     }
 
--- Sedan.java
+++ Sedan.java
@@ -1,9 +1,10 @@
 import java.awt.*;
 import java.awt.geom.*;
-public class Sedan extends Car implements DrawingObject{
+public class Sedan extends Car{
     private double x, y, width, height, size;
     private Color color;
     private Square body, window, roof;
+    private Line front_left_pillar, front_right_pillar, middle_pillar, back_left_pillar, back_right_pillar;
     public Sedan(double x, double y, double size, Color color) {
         this.x = x;
         this.y = y;
@@ -13,10 +14,20 @@ public class Sedan extends Car implements DrawingObject{
         body = new Square(x, y, width, height, color);
         window = new Square(x+size*0.1, y+size*.6, width*0.8, height*7/12, Color.BLACK);
         roof = new Square(x+size*0.2, y+size, width*0.6, height*1/3, color);
+        front_left_pillar = new Line(x+size*0.1, y+size*0.6, x+size*0.2, y+size, 2, color);
+        front_right_pillar = new Line(x+size*0.9, y+size*0.6, x+size*0.8, y+size, 2, color);
+        middle_pillar = new Line(x+size*0.1, y+size*1.4, x+size*0.9, y+size*1.4, 2, color);
+        back_left_pillar = new Line(x+size*0.1, y+size*2, x+size*0.2, y+size*1.8, 2, color);
+        back_right_pillar = new Line(x+size*0.9, y+size*2, x+size*0.8, y+size*1.8, 2, color);
     }
     public void draw(Graphics2D g2d){
         body.draw(g2d);
         window.draw(g2d);
         roof.draw(g2d);
+        front_left_pillar.draw(g2d);
+        front_right_pillar.draw(g2d);
+        middle_pillar.draw(g2d);
+        back_left_pillar.draw(g2d);
+        back_right_pillar.draw(g2d);
     }
 }
