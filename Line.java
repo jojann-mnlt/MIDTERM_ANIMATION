71deafc37be5fbdd6b5dffc87309566cd9d57f29
jcb24 <jacoblb237@gmail.com>
2026-02-17 15:54:46 +0800

    Line.java

--- Line.java
+++ Line.java
@@ -2,40 +2,25 @@ import java.awt.*;
 import java.awt.geom.*;
 
 public class Line implements DrawingObject {
-    private double x, y, x2, y2, size;
+    private double x, y, x2, y2;
     private Color color;
 
-    public Line(double x, double y, double x2, double y2, double size, Color color) {
+    public Line(double x, double y, double x2, double y2, Color color) {
         this.x = x;
         this.y = y;
-
         this.x2 = x2;
         this.y2 = y2;
-        this.size = size;
+        this.color = color;
     }
 
-
     public void draw(Graphics2D g2d) {
-        Path2D.Double line = new Path2D.Double();
-        line.moveTo(x, y);
-        line.lineTo(x2, y2);
+        Line2D.Double line = new Line2D.Double(x, y, x2, y2);
         g2d.setColor(color);
         g2d.draw(line);
     }
 
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void moveX(double amount) {
-        x += amount;
-    }
-
-    public void moveY(double amount) {
-        y += amount;
-    }
+    public double getX() {return x;}
+    public double getY() {return y;}
+    public void moveX(double amount) {x += amount;}
+    public void moveY(double amount) {y += amount;}
 }
