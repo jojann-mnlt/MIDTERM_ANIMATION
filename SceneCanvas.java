f5b25816e4c16fdffcb48dcaf147d6d98d3df8ee
jojann-mnlt <johannmanulat128@gmail.com>
2026-02-20 17:48:49 +0800

    added additional detail to the gear stick canvas

--- SceneCanvas.java
+++ SceneCanvas.java
@@ -3,5 +3,22 @@ import java.awt.*;
 import javax.swing.*;
 
 public class SceneCanvas extends JComponent {
+    private double width, height;
 
+    public SceneCanvas(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public SceneCanvas() {
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+        Rectangle2D.Double background = new Rectangle2D.Double(0, 0, width, height);
+        g2d.setColor(Color.GREEN);
+        g2d.fill(background);
+    }
 }
